d7b0c406902dc9ddd28f55c87d2a3fbd66f8fe85
Tristan <tkidder132@gmail.com>
2015-02-04 00:52:11 -0600

    Buttons
    Buttons are now like google buttons, feeling lucky works now and
    apparently didn't before this.

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 This is a mock Google homepage developed while going through The Odin Project course.
+
+I borrowed the google buttons css and html from here: http://cssdeck.com/labs/google-buttons
--- google.js
+++ google.js
@@ -19,3 +19,27 @@ function searchGoogle()
 	
 	window.open(searchURL,"_self");
 }
+
+function feelingLucky()
+{
+	var x = document.getElementById("txtSearch");
+	var test = x.value;
+	
+	var searchURL = "https://www.google.com/search?q=";
+	
+	for( var i = 0; i < test.length; i++ )
+	{
+		if( test.substring(i,i+1) === " " )
+		{
+			searchURL += "+";
+		}
+		else
+		{
+			searchURL += test.substring(i,i+1);
+		}
+	}
+	
+	searchURL += "&btnI"
+	
+	window.open(searchURL,"_self");
+}
